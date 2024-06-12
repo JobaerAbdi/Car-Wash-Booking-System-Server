a462039a6d3eba94b8a6daa6a3bb1573436e7a14
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-12 21:46:10 +0600

    run the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,3 @@ export default [
   ...tseslint.configs.recommended,
   pluginReactConfig,
 ];
-
--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 
 app.get("/", (req: Request, res: Response) => {
-const result = "Hello !!!"
+const result = "Hello Car Wash Center !!!"
   res.send(result);
 });
 
