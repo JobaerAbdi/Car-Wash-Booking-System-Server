c96e51f43cd721c30f38f5c4e1ab6551684fdc7e
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-12 21:52:06 +0600

    all-setup-comlate-commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import router from "./app/routes";
 
 const app: Application = express();
 
@@ -10,7 +11,8 @@ app.use(cors());
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-
+//application routes
+app.use("/api/v1" , router);
 
 app.get("/", (req: Request, res: Response) => {
 const result = "Hello Car Wash Center !!!"
