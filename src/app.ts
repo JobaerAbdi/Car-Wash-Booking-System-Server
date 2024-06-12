cbc3fa1794e33531ce65648aeb5c59d952543553
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-12 21:33:02 +0600

    all-tools-install-commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+
+
+app.get("/", (req: Request, res: Response) => {
+const result = "Hello !!!"
+  res.send(result);
+});
+
+
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join((process.cwd(), ".env")) });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  prot: process.env.USER_PROT,
+  database_url: process.env.DATABASE_URL,
+  data_salt_rounds : process.env.DATA_SALT_ROUNDS,
+  default_password:process.env.DEPFULT_PASS
+};
