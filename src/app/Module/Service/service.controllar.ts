ba558034f4be95886510595b4fb8ffff3cbd199a
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-18 19:00:40 +0600

    get-all-services-commit

--- src/app/Module/Service/service.controllar.ts
+++ src/app/Module/Service/service.controllar.ts
@@ -1,6 +1,9 @@
+import httpStatus from "http-status";
+import AppError from "../../Error/AppError";
 import catchAsync from "../../utils/catchAsync";
 import { ServiceServices } from "./service.service";
 
+//create service
 const createServiceDb = catchAsync(async (req, res) => {
   const movieData = req.body;
   const result = await ServiceServices.createService(movieData);
@@ -11,6 +14,32 @@ const createServiceDb = catchAsync(async (req, res) => {
   });
 });
 
+//getBId Service
+const getServiceByIdDB = catchAsync(async (req, res) => {
+  const { serviceId } = req.params;
+  if (!serviceId) {
+    throw new AppError(httpStatus.NOT_FOUND, "Service ID not found");
+  }
+  const result = await ServiceServices.getServiceById(serviceId);
+  res.json({
+    success: true,
+    message: "Service retrieved successfully",
+    data: result,
+  });
+});
+
+//get all
+const getAllServiceDB = catchAsync(async (req, res) => {
+  const result = await ServiceServices.getAllService();
+  res.status(200).json({
+    success: true,
+    message: "Services retrieved successfully",
+    data: result,
+  });
+});
+
 export const ServiceControllars = {
   createServiceDb,
+  getServiceByIdDB,
+  getAllServiceDB
 };
--- src/app/Module/Service/service.service.ts
+++ src/app/Module/Service/service.service.ts
@@ -6,6 +6,21 @@ const createService = async (ServiceData: CService) => {
   return result;
 };
 
+//get by Id Service
+const getServiceById = async (id: string) => {
+  const result = await Service.findById(id);
+  return result;
+};
+
+//get all service
+const getAllService = async() =>{
+  const result = await Service.find();
+  return result
+};
+
+
 export const ServiceServices = {
   createService,
+  getServiceById,
+  getAllService
 };
