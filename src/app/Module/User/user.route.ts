2032ade84f10f96c984921c2008cb8ced4aa22af
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-18 16:14:42 +0600

    validaed-requiest-create-commit

--- src/app/Module/User/user.route.ts
+++ src/app/Module/User/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { UserControllars } from "./user.controllar";
+import validationRequest from "../../middlewares/validaedRequest";
+import { UserValidation } from "./user.validation";
 
 const router = express.Router();
 
-router.post("/signup", UserControllars.createUserDB);
+router.post("/signup",validationRequest(UserValidation.createUserValidationSchema), UserControllars.createUserDB);
 
 export const UserRoutes = router;
---
+++ src/app/Module/User/user.validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const createUserValidationSchema = z.object({
+  name: z.string(),
+  email: z.string().email({ message: "Email is invalid" }),
+  password: z.string(),
+  phone: z.string(),
+  role: z.enum(["admin", "user"]),
+  address: z.string(),
+});
+
+export const UserValidation = {
+  createUserValidationSchema,
+};
