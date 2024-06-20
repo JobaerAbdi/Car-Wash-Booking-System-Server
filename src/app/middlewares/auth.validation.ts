2ad981a6a124fd6e84feeb7cdd079f2550b8a62f
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-20 17:50:59 +0600

    update-commit

--- src/app/middlewares/auth.validation.ts
+++ src/app/middlewares/auth.validation.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from "../config";
-import { JwtPayload } from "../Interface/Jwt";
 import catchAsync from "../utils/catchAsync";
 import httpStatus from "http-status";
 import AppError from "../Error/AppError";
 import { CUserRole } from "../Module/User/user.interface";
+import { User } from "../Module/User/user.model";
 
 
 export const AuthValidated = (...requierdRole: CUserRole[]) => {
@@ -14,33 +14,17 @@ export const AuthValidated = (...requierdRole: CUserRole[]) => {
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
     }
-
-    console.log(token)
     const decoded = jwt.verify(token.split(' ')[1], config.jwt_access_secret as string) as JwtPayload;
-    const { role, userId, iat } = decoded;
+    const { userId, role, iat } = decoded;
 
-    const isExistsUser = await User.isUserExsitsByCustomId(userId);
+    const isExistsUser = await User.findById(userId);
+    console.log(isExistsUser)
+
     if (!isExistsUser) {
       throw new AppError(httpStatus.NOT_FOUND, "This user is not found!");
     }
-
-    // Check user status
-    if (isExistsUser.isDeleted) {
-      throw new AppError(httpStatus.FORBIDDEN, "This user is deleted!");
-    }
-
-    if (isExistsUser.status === "blocked") {
-      throw new AppError(httpStatus.FORBIDDEN, "This user is blocked!");
-    }
-
-    if (
-      isExistsUser.passwordChangeAt &&
-      User.isJWTIssuseBeforePasswoedChange(isExistsUser.passwordChangeAt, iat as number)
-    ) {
-      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
-    }
-
-    if (requierdRole && !requierdRole.includes(role)) {
+  
+    if (!(requierdRole[0] === isExistsUser.role)) {
       throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
     }
 
