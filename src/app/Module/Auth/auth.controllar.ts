9150fc5733ba4fe82b4b59832a8af30f9df3629f
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-20 16:26:49 +0600

    user-login-successfully-commit

--- src/app/Module/Auth/auth.controllar.ts
+++ src/app/Module/Auth/auth.controllar.ts
@@ -1,25 +1,22 @@
 import httpStatus from "http-status";
 import config from "../../config";
 import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
 
 const loginUserDB = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
 
-  const { refreshToken, accessToken } = result;
+  const { user, refreshToken, accessToken } = result;
   res.cookie("refreshToken", refreshToken, {
     secure: config.NODE_ENV === "production",
-    httpOnly: true
+    httpOnly: true,
   });
-
-  sendResponse(res, {
+  res.status(httpStatus.OK).json({
     statusCode: httpStatus.OK,
     success: true,
     message: "User is logged in successfully",
-    data: {
-      accessToken
-    },
+    token: accessToken,
+    data: user,
   });
 });
 
--- src/app/Module/Auth/auth.service.ts
+++ src/app/Module/Auth/auth.service.ts
@@ -5,40 +5,42 @@ import { CLoginUser } from "./auth.interface";
 import config from "../../config";
 import { createToken, isPasswordMatched } from "./auth.utils";
 
-
 const loginUser = async (payload: CLoginUser) => {
   const user = await User.findOne({ email: payload.email }).select("+password");
-    if (!user) {
-      throw new AppError(httpStatus.NOT_FOUND, "This user is Not Found !!");
-    }
-  
-    // Verify password
-    const passwordMatch = await isPasswordMatched(
-      payload.password,
-      user.password
-    );
-  
-    if (!passwordMatch) {
-      throw new Error("Password not matched");
-    }
-  
-  
-    // Create token and send to the user
-    const jwrPayload = {
-      userId: user.id,
-      role: user.role,
-    };
-    const accessToken = createToken(jwrPayload, config.jwt_access_secret as string, config.jwt_assess_exrpired as string)
-  
-  
-    const refreshToken = createToken(jwrPayload, config.jwt_refreshtoken as string, config.jwt_refresh_exrpired as string)
-  
-  
-    // const needsPasswordChange = isExistsUser?.needsPasswordChange;
-    return { accessToken, refreshToken };
-  };
-  
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, "This user is Not Found !!");
+  }
 
-  export const AuthServices = {
-    loginUser
+  // Verify password
+  const passwordMatch = await isPasswordMatched(
+    payload.password,
+    user.password
+  );
+
+  if (!passwordMatch) {
+    throw new Error("Password not matched");
+  }
+
+  // Create token and send to the user
+  const jwrPayload = {
+    userId: user.id,
+    role: user.role,
   };
+  const accessToken = createToken(
+    jwrPayload,
+    config.jwt_access_secret as string,
+    config.jwt_assess_exrpired as string
+  );
+
+  const refreshToken = createToken(
+    jwrPayload,
+    config.jwt_refreshtoken as string,
+    config.jwt_refresh_exrpired as string
+  );
+
+  return { user, accessToken, refreshToken };
+};
+
+export const AuthServices = {
+  loginUser,
+};
